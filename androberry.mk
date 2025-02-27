d8aacefd82d449f2608277274d786b4ecd705c2f
Joao Danilo <joaodanilo1992@gmail.com>
2025-02-26 21:49:58 -0400

    Adding required content for the lunch command to succeed

---
+++ androberry.mk
@@ -0,0 +1,9 @@
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+
+# ================= Production Options =================
+PRODUCT_NAME            := androberry
+PRODUCT_DEVICE          := androberryboard
+PRODUCT_BRAND           := generic
+PRODUCT_MODEL           := XPTO
+PRODUCT_MANUFACTURER    := jao
+# ================= END =================
