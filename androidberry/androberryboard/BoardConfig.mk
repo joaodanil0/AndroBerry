f578dc4b2bcb823672328a8ab98f5dbb6df18afd
Joao Danilo <joaodanilo1992@gmail.com>
2025-03-05 20:02:37 -0400

    Fixex libgbm_mesa_wrapper error

--- androidberry/androberryboard/BoardConfig.mk
+++ androidberry/androberryboard/BoardConfig.mk
@@ -12,3 +12,10 @@ TARGET_2ND_CPU_ABI		:= armeabi-v7a
 TARGET_2ND_CPU_ABI2		:= armeabi
 TARGET_2ND_CPU_VARIANT	:= generic
 # ================= END =================
+
+# error: "libgbm_mesa_wrapper (SHARED_LIBRARIES android-arm64) 
+# missing libgbm_mesa (SHARED_LIBRARIES android-arm64)"
+# https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/10183?commit_id=8621bd8d5e67173281c6a6dd64054d4b0366f023#23d9dc374eea33a36aee5ab6431635c4ddbf10ca
+# https://github.com/raspberry-vanilla/android_external_mesa3d
+BOARD_MESA3D_BUILD_LIBGBM := true
+BOARD_MESA3D_USES_MESON_BUILD := true
